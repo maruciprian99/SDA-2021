81464d68207ef9daf00a95e4f7ecfc160f7ddbf1
Maru Ciprian <79523727+maruciprian99@users.noreply.github.com>
2021-04-02 14:24:15 +0300

    temperatura

--- teme/tema2/temperatura/src/temps/Temp.java
+++ teme/tema2/temperatura/src/temps/Temp.java
@@ -7,14 +7,14 @@ public class Temp {
 		
 		double arr[] = {36.5 , 36.3 , 36.9 , 37.1 , 38.5};
 		int n=arr.length;
-		System.out.print("Temperaturi normale:");
+		System.out.print("Temperaturi anormale:");
 		for(int i=0;i<n;i++)
 		{
 			if(arr[i]>37)
 				 System.out.print( "("+arr[i] +"," +i + ")" + " ; ");
 		}
 		System.out.println();
-		System.out.print("Temperaturi anormale: ");
+		System.out.print("Temperaturi normale: ");
 		
 		for(int i=0;i<n;i++)
 		{
